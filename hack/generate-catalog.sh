9996ffc09369d963af85efbdfff4a64b435fabbd
nicolaferraro <ni.ferraro@gmail.com>
2020-09-09 14:02:46 +0200

    Generate full catalog

--- hack/generate-catalog.sh
+++ hack/generate-catalog.sh
@@ -3,4 +3,22 @@
 location=$(dirname $0)
 rootdir=$location/../
 
-echo TODO
+
+cd $rootdir
+
+echo "{\"kind\": \"List\", \"apiVersion\": \"v1\", \"items\": [" > catalog.tmp
+
+first=true
+for f in `ls *.kamelet.yaml | sort -V`; do
+    if [ "$first" = false ]; then
+        echo "," >> catalog.tmp
+    fi
+    cat $f | yq . >> catalog.tmp
+    first=false
+done
+
+echo "]}" >> catalog.tmp
+
+# format and finalize
+cat catalog.tmp | jq > catalog.json
+rm catalog.tmp
